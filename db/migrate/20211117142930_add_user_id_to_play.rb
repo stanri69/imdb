d6d0d944d27f9aa53be437110d4921c507f6032e
stanri69 <stanri69@ukr.net>
2021-11-17 16:34:33 +0200

    Add Navigation and Associations

---
+++ db/migrate/20211117142930_add_user_id_to_play.rb
@@ -0,0 +1,5 @@
+class AddUserIdToPlay < ActiveRecord::Migration[6.1]
+  def change
+    add_column :plays, :user_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_17_131919) do
+ActiveRecord::Schema.define(version: 2021_11_17_142930) do
 
   create_table "plays", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_11_17_131919) do
     t.string "director"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
